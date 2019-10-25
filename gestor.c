1f851d69d56a6a115ae160a8de52908ececbe44b
PatinhoGamer <rodrigocm1999@gmail.com>
2019-10-26 00:38:51 +0100

    a lot has been done

--- gestor.c
+++ gestor.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <signal.h>
 #include <unistd.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <sys/file.h>
+#include <sys/stat.h>
 #include <pthread.h>
 #include "utils.h"
 
@@ -24,7 +26,7 @@ Message* new_Message(){
 	return obj;
 }
 
-int acquireLock (char *fileSpec) {
+int fileLock (char *fileSpec) {
     int lockFd;
 
     if ((lockFd = open (fileSpec, O_CREAT | O_RDWR, 0600))  < 0){
@@ -34,13 +36,7 @@ int acquireLock (char *fileSpec) {
         close (lockFd);
         return -1;
     }
-
     return lockFd;
-}
-
-void releaseLock (int lockFd) {
-    flock (lockFd, LOCK_UN);
-    close (lockFd);
 }
 
 void printTopics(Node* head){
@@ -76,52 +72,99 @@ void printMsgs(Node* head){
 	}
 }
 
+void verifyBadWords(){
+	char msgEnd[] = "##MSGEND##";
+}
+
+typedef struct {
+	int id;
+	char pathFIFO[32];
+}NewClientInfo;
+
+void* handleClient(void* data){
+	NewClientInfo * info = data;
+	
+	//Dont forget to free it
+	free(info);
+}
+
+void* waitForNewClients(void* data){
+	//char mainFIFO[] = "./fifos/listener";
+	char mainFIFO[] = "/tmp/gestorListenerFifo";
+	int fifo = open(mainFIFO,O_RDONLY);
+	if(fifo == -1){
+		mkfifo(mainFIFO,0666);
+		fifo = open(mainFIFO,O_RDONLY);
+		if(fifo == -1 ){
+			printf("Main client listener creation error (FIFO)\n");
+			printf("Error: %d\n",errno);
+			exit(0);
+		}
+	}
+	int const bufferSize = 128;
+	char buffer[bufferSize];
+	while(1){
+		read(fifo, buffer, bufferSize * sizeof(char));
+		
+		NewClientInfo * info = malloc(sizeof(NewClientInfo)); ;
+		char* piece = strtok(buffer,",");
+		info->id = atoi(piece);
+		piece = strtok(buffer,",");
+		strcpy(info->pathFIFO,piece);
+
+		pthread_t thread;
+		pthread_create(&thread,NULL,handleClient,(void*)info);
+	}
+
+}
+
+void shutdown(int signal){
+	printf("Exiting\n");
+	exit(0);
+}
+
 int main(int argc,char* argv[]){
 
-	printf("Starting\n");
-
+	//try to lock file to avoid multiple processes to run
 	char filePath[100] =  "./lock.lock";
-	int lock = acquireLock(filePath);
+	int lock = fileLock(filePath);
 	if (lock == -1){
 		printf("Program already running\nExiting");
 		exit(0);
 	}
 
+	//start verificador
 	int verifPipe[2];
 	pipe(verifPipe);
 	int childPid = fork();
-
 	if(childPid == 0){
-		//Child
 		close(0);
 		dup(verifPipe[0]);
 		close(verifPipe[0]);
 		close(verifPipe[1]);
 
-		execl("./verificador","verificador",NULL);
-	}else{
-		//Parent
-		close(verifPipe[0]);
-		char str[100];
-		fprintf(verifPipe[1],str,strlen(str));
+		char badWordsFile[] = "badwords.txt";
+		char ver[] = "verificador";
+		execl(ver, ver, badWordsFile, (char*)NULL);
+		printf("Bad Word Verifier Not started.");
+		exit(0);
+		//Exec gave error
 	}
 
+	signal(SIGINT, shutdown);
 
-	pthread_t mainThread;
-	pthread_create(mainThread,NULL,/*function pointer*/NULL,
-	/*void* of the fucntion*/NULL);
-
-
-	printf("Has started\n");
+	pthread_t listenerThread;
+	pthread_create(&listenerThread,NULL,waitForNewClients,(void*)NULL);
 
 	Node* msgsHead = new_Node(NULL);
 	Node* topicsHead = new_Node(NULL);
 	Node* usersHead = new_Node(NULL);
+	int filter = 0;
 	/*List* msgsHead = new_List();
 	List* topicsHead = new_List();
 	List* usersHead = new_List();*/
 
-	int filter = 0;
+	printf("Has started\n");
 
 	printf("Write \"help\" to get command information\n");
 
@@ -132,50 +175,76 @@ int main(int argc,char* argv[]){
 		cmd = strtok(command," ");
 		
 		if(strcmp(cmd,"filter") == 0){
-			char* token = strtok(command," ");
-			if(strcmp(token,"on") == 0){
-				filter = 1;
-			}else if(strcmp(token,"off") == 0){
-				filter = 0;
+			char* token = strtok(NULL," ");
+			printf("asddsa\n \tTest: %s \n",token);
+			if(token != NULL){
+				if(strcmp(token,"on") == 0){
+					filter = 1;
+					printf("Filter is on\n");
+				}else if(strcmp(token,"off") == 0){
+					filter = 0;
+					printf("Filter is off\n");
+				}else if(strcmp(token,"status") == 0){
+					if(filter == 0) printf("Filter is off\n");
+					else printf("Filter is on\n");
+				}
 			}else{
 				printf("\nInvalid filter option, available : on off\n");
 			}
+			printf("asddsa 2\n");
 		}
+
+
 		if(strcmp(cmd,"users") == 0){
 			printUsers(usersHead);
 		}
+
+
 		else if(strcmp(cmd,"topics") == 0){
 			printUsers(topicsHead);
 		}
+
+
 		else if(strcmp(cmd,"msg") == 0){
 			printMsgs(msgsHead);
 		}
+
+
 		else if(strcmp(cmd,"topic") == 0){
 			char* topic = strtok(command," ");
 			Node* curr = msgsHead;
 			while( curr != NULL ){
 				Message* currMessage = (Message*)curr->data;
 				if(strcmp(currMessage->topic,topic) == 0){
-					printf("");
+					
 				}
 
 				curr = curr->next;
 			}
-
 		}
-		else if(strcmp(cmd,"del")){
+
+
+		else if(strcmp(cmd,"del") == 0){
 			char* token = strtok(command," ");
 		}
-		else if(strcmp(cmd,"shutdown")){
-			printf("Exiting\n");
-			exit(0);	
+
+
+		else if(strcmp(cmd,"shutdown") == 0){
+			shutdown(SIGINT);
 		}
-		else if(strcmp(cmd,"help")){
-			//open and print help.txt
+
+
+		else if(strcmp(cmd,"help") == 0){
+			FILE* file = fopen("help.txt","r");
+			int const bufferSize = 1024;
+			char buffer[bufferSize];
+			int sdas = fread(buffer,sizeof(char),bufferSize,file);
+			printf("\n");
+			write(0,buffer,strlen(buffer));
+			printf("\n\n");
 		}
+
 
 	}
-
-	while(1) {pause();}
 	return 0;
 }
