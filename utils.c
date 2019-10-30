6975b4e04e6bbc234dc32723a6176d18fd32ad4f
PatinhoGamer <rodrigocm1999@gmail.com>
2019-10-30 23:38:26 +0000

    havia um oun 2 erros no utils.c

--- utils.c
+++ utils.c
@@ -41,9 +41,9 @@ Node* LinkedList_pop(LinkedList* list,void* obj){
 }
 
 int LinkedList_getSize(LinkedList* list){
-	int size = 1;
+	int size = 0;
 	Node* curr = list->head;
-	while(curr->next != NULL){
+	while(curr != NULL){
 		++size;
 		curr = curr->next;
 	}
